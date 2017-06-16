9c305ca4b481d386d4218dc820a62e48490d6257
alban <alban.carroue@gmail.com>
2017-06-16 14:47:45 +0200

    premier commit

---
+++ src/AppBundle/Controller/PlayerController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class PlayerController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $title = "Liste des joueurs";
+
+        $joueur1 = ['nom' => 'Bonnucci', 'prenom' => 'Leo', 'age' => 29];
+        $joueur2 = ['nom' => 'Chiellini', 'prenom' => 'Giorgio', 'age' => 34];
+        $joueur3 = ['nom' => 'Barzagli', 'prenom' => 'Andréa', 'age' => 36];
+    
+        $joueurs = [$joueur1, $joueur2, $joueur3];
+
+        return $this->render('player/index.html.twig', array(
+            'title'     => $title,
+            'message'   => 'Symfony semble formidable',
+            'joueur1'   => $joueur1,
+            'joueurs'   => $joueurs
+        ));
+    }
+}
